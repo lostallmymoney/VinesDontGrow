bfc8ba0600fc213ea0f31f9ba975e100a888a460
Cam <f-rog@hotmail.com>
2022-01-09 18:29:33 -0500

    Add files via upload

--- src/main/java/vinesDontGrow/mixin/VineBlockMixin.java
+++ src/main/java/vinesDontGrow/mixin/VineBlockMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(VineBlock.class)
 public abstract class VineBlockMixin {
     @Inject(method = "randomTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V", at = @At("HEAD"), cancellable = true)
-    private void randomTick(CallbackInfo ci){
+    private void onRandomTick(CallbackInfo ci){
         ci.cancel();
     }
 }
